7b194f41c446448d5eb8abd22d7641df86c77717
hallymer <mu072468@gmail.com>
2020-09-20 08:38:57 +0900

    주석부분 표현 충돌 해결

--- calc.py
+++ calc.py
@@ -1,10 +1,10 @@
 # add program
-def sum_function( n1 , n2):
+def sum_function( n1 , n2): # 두 정수의 합을 구하는 함수
     return n1 + n2
 
 num1 = int ( input("input number 1") )
 num2 = int ( input("input number 2") )
 
-sum = num1 + num2
+sum = sum_function( num1 , num2)
 
 print(num1 , "+" , num2 , "=" , sum )
