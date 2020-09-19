f75ead1c17f9998bd6629a89f92f28a580f8c2d1
hallymer <mu072468@gmail.com>
2020-09-20 08:36:42 +0900

    insert commit about calc.py [ insert ]

--- calc.py
+++ calc.py
@@ -1,10 +1,10 @@
 # add program
-def sum_function( n1 , n2):
+def sum_function( n1 , n2): # add two number
     return n1 + n2
 
 num1 = int ( input("input number 1") )
 num2 = int ( input("input number 2") )
 
-sum = num1 + num2
+sum = sum_function( num1 , num2)
 
 print(num1 , "+" , num2 , "=" , sum )
