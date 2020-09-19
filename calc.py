ffebeb383b63640c13e75f8ff63bb8381fdd80ab
hallymer <mu072468@gmail.com>
2020-09-20 08:32:03 +0900

    insert def sum_function about calc.py [ insert ]

--- calc.py
+++ calc.py
@@ -1,7 +1,10 @@
 # add program
-num1 = int ( input(“ input number 1” ) )
-num2 = int ( input(“ input number 2” ) )
+def sum_function( n1 , n2):
+    return n1 + n2
+
+num1 = int ( input("input number 1") )
+num2 = int ( input("input number 2") )
 
 sum = num1 + num2
 
-print(num1 , “+” , num2 ,  “=” , sum )
+print(num1 , "+" , num2 , "=" , sum )
